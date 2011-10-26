c4e9e4614cf9b326edc468a9a15c99f557494418
Christian Kellner <kellner@bio.lmu.de>
2011-10-26 21:42:40 +0200

    Move html to own html_template module

---
+++ IPNotizblock/html_templates.py
@@ -0,0 +1,124 @@
+IPYTHON_NOTEBOOK_HTML="""
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>IPython Notebook</title>
+<link rel="stylesheet" href="static/jquery/css/themes/aristo/jquery-wijmo.css" type="text/css" />
+<!-- <link rel="stylesheet" href="static/jquery/css/themes/rocket/jquery-wijmo.css" type="text/css" /> -->
+<!-- <link rel="stylesheet" href="static/jquery/css/themes/smoothness/jquery-ui-1.8.14.custom.css" type="text/css" />-->
+<!-- <script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML" charset="utf-8"></script> -->
+<script type='text/javascript' src='static/mathjax/MathJax.js?config=TeX-AMS_HTML' charset='utf-8'></script>
+<script type="text/javascript">
+function CheckMathJax(){
+var div=document.getElementById("MathJaxFetchingWarning")
+if(window.MathJax){
+document.body.removeChild(div)
+}
+else{
+div.style.display = "block";
+}
+}
+if (typeof MathJax == 'undefined') {
+console.log("No local MathJax, loading from CDN");
+document.write(unescape("%3Cscript type='text/javascript' src='http://cdn.mathjax.org/mathjax/latest/MathJax.js%3Fconfig=TeX-AMS_HTML' charset='utf-8'%3E%3C/script%3E"));
+}else{
+console.log("Using local MathJax");
+}
+</script>
+<link rel="stylesheet" href="static/codemirror/lib/codemirror.css">
+<link rel="stylesheet" href="static/codemirror/mode/markdown/markdown.css">
+<link rel="stylesheet" href="static/codemirror/mode/rst/rst.css">
+<link rel="stylesheet" href="static/codemirror/theme/ipython.css">
+<link rel="stylesheet" href="static/codemirror/theme/default.css">
+<link rel="stylesheet" href="static/prettify/prettify.css"/>
+<link rel="stylesheet" href="static/css/boilerplate.css" type="text/css" />
+<link rel="stylesheet" href="static/css/layout.css" type="text/css" />
+<link rel="stylesheet" href="static/css/base.css" type="text/css" />
+<link rel="stylesheet" href="static/css/notebook.css" type="text/css" />
+<link rel="stylesheet" href="static/css/renderedhtml.css" type="text/css" />
+</head>
+<body onload='CheckMathJax();' data-project=/home/gicmo data-notebook-id=@NOTEBOOK_ID@ data-base-project-url=/ data-base-kernel-url=/>
+<div id="header" style="display: none; height: 0px;">
+<span id="save_widget" style="display: none;">
+<input type="text" id="notebook_name" size="20" style="display: none;"></textarea>
+<span id="notebook_id" style="display:none">@NOTEBOOK_ID@</span>
+<button style="display: none; id="save_notebook"><u>S</u>ave</button>
+</span>
+<span id="kernel_status" style="display: none;">Idle</span>
+</div>
+<div id="MathJaxFetchingWarning"
+style="width:80%; margin:auto;padding-top:20%;text-align: justify; display:none">
+<p style="font-size:26px;">There was an issue trying to fetch MathJax.js
+from the internet.</p>
+<p style="padding:0.2em"> With a working internet connection, you can run
+the following at a Python or IPython prompt, which will install a local
+copy of MathJax:</p>
+<pre style="background-color:lightblue;border:thin silver solid;padding:0.4em">
+from IPython.external import mathjax; mathjax.install_mathjax()
+</pre>
+This will try to install MathJax into the directory where you installed
+IPython. If you installed IPython to a location that requires
+administrative privileges to write, you will need to make this call as
+an administrator. On OSX/Linux/Unix, this can be done at the
+command-line via:
+<pre style="background-color:lightblue;border:thin silver solid;padding:0.4em">
+sudo python -c "from IPython.external import mathjax; mathjax.install_mathjax()"
+</pre>
+</p>
+</div>
+<div id="main_app">
+
+<div id="left_panel_splitter" style="display:none"></div>
+<div id="notebook_panel">
+<div id="notebook"></div>
+<div id="pager_splitter"></div>
+<div id="pager"></div>
+</div>
+</div>
+<script src="static/jquery/js/jquery-1.6.2.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/jquery/js/jquery-ui-1.8.14.custom.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/jquery/js/jquery.autogrow.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/codemirror/lib/codemirror.js" charset="utf-8"></script>
+<script src="static/codemirror/mode/python/python.js" charset="utf-8"></script>
+
+<script src="static/codemirror/mode/htmlmixed/htmlmixed.js" charset="utf-8"></script>
+<script src="static/codemirror/mode/xml/xml.js" charset="utf-8"></script>
+<script src="static/codemirror/mode/javascript/javascript.js" charset="utf-8"></script>
+<script src="static/codemirror/mode/css/css.js" charset="utf-8"></script>
+<script src="static/codemirror/mode/rst/rst.js" charset="utf-8"></script>
+<script src="static/codemirror/mode/markdown/markdown.js" charset="utf-8"></script>
+<script src="static/pagedown/Markdown.Converter.js" charset="utf-8"></script>
+<script src="static/prettify/prettify.js" charset="utf-8"></script>
+<script src="static/js/namespace.js" type="text/javascript" charset="utf-8"></script>
+
+<script src="static/js/utils.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/cell.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/codecell.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/textcell.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/kernel.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/kernelstatus.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/layout.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/savewidget.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/quickhelp.js" type="text/javascript" charset="utf-8"></script>
+
+<script src="static/js/pager.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/panelsection.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/printwidget.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/leftpanel.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/notebook.js" type="text/javascript" charset="utf-8"></script>
+<script src="static/js/notebookmain.js" type="text/javascript" charset="utf-8"></script>
+</body>
+</html>
+"""
+
+
+ABOUT_PAGE = """
+<html><head><title>IPython Notebook Gtk Shell</title></head><body>
+<h1>Welcome to Lecture Title Excercise Class</h1>
+<p>This is a cool intro Text that we need to come up with ...<br/>
+</p>
+<span id='notebook_id' style='dsiplay:none;'></span>
+<input id='notebook_name' style='display:none;'/>
+</body></html>
+"""
